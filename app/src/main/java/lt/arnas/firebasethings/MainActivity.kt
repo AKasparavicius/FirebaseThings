8c453b06f4837668b527d7649a524efb25812d8f
AKasparavicius <kasparavicius.ar@gmail.com>
2023-09-22 17:26:26 +0300

    Set up binding and firebaseAuth in MainActivity

--- app/src/main/java/lt/arnas/firebasethings/MainActivity.kt
+++ app/src/main/java/lt/arnas/firebasethings/MainActivity.kt
@@ -2,10 +2,22 @@ package lt.arnas.firebasethings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import lt.arnas.firebasethings.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
     }
 }
