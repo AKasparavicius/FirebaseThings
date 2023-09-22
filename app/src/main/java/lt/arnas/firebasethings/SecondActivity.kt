6f3aca3677936036f8b3079a7f4f19ef2fb3cc10
AKasparavicius <kasparavicius.ar@gmail.com>
2023-09-22 18:34:21 +0300

    Add binding and firebaseAuth to secondActivity

--- app/src/main/java/lt/arnas/firebasethings/SecondActivity.kt
+++ app/src/main/java/lt/arnas/firebasethings/SecondActivity.kt
@@ -2,10 +2,22 @@ package lt.arnas.firebasethings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.google.firebase.auth.FirebaseAuth
+import lt.arnas.firebasethings.databinding.ActivityMainBinding
+import lt.arnas.firebasethings.databinding.ActivitySecondBinding
+
+private lateinit var binding: ActivitySecondBinding
+private lateinit var firebaseAuth: FirebaseAuth
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
     }
 }
