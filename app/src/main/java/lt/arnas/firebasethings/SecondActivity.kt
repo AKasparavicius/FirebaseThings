895538d822c8b1316bdf24499fe73fe7f5a05c01
AKasparavicius <kasparavicius.ar@gmail.com>
2023-09-22 19:57:56 +0300

    Set up register/second activity

--- app/src/main/java/lt/arnas/firebasethings/SecondActivity.kt
+++ app/src/main/java/lt/arnas/firebasethings/SecondActivity.kt
@@ -1,8 +1,10 @@
 package lt.arnas.firebasethings
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import lt.arnas.firebasethings.databinding.ActivityMainBinding
 import lt.arnas.firebasethings.databinding.ActivitySecondBinding
@@ -19,5 +21,37 @@ class SecondActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
+
+        signUpButton()
+
+    }
+
+    fun signUpButton() {
+        binding.confirmRegisterBtn.setOnClickListener {
+            val email = binding.emailFieldRgstr.text.toString()
+            val password = binding.passwordFieldRgstr.text.toString()
+            val passwordConfirm = binding.passwordFieldCnfrm.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() && passwordConfirm.isNotEmpty()) {
+                if (password.equals(passwordConfirm)) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener {
+                            if(it.isSuccessful){
+                                val intent = Intent(this, MainActivity::class.java)
+                                startActivity(intent)
+                            } else {
+                                Toast.makeText(this, it.exception.toString(),
+                                    Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                } else {
+                    Toast.makeText(this, "Passwords are not matching!",
+                        Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "One or more fields are empty!",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
