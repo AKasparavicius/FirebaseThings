e13369ad9f5861ccb46cd8301c18439b7d062f38
AKasparavicius <kasparavicius.ar@gmail.com>
2023-10-02 22:13:13 +0300

    Added profile fragment design

--- app/src/main/java/lt/arnas/firebasethings/fragments/ProfileFragment.kt
+++ app/src/main/java/lt/arnas/firebasethings/fragments/ProfileFragment.kt
@@ -1,9 +1,11 @@
 package lt.arnas.firebasethings.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
+import lt.arnas.firebasethings.MainActivity
 import lt.arnas.firebasethings.R
 import lt.arnas.firebasethings.databinding.FragmentProfileBinding
 
@@ -19,5 +21,18 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
+        binding.userEmail.text = firebaseAuth.currentUser?.email
+
+        logOut()
+
+    }
+
+    private fun logOut() {
+        binding.logoutBtn.setOnClickListener {
+            firebaseAuth.signOut()
+            val intent = Intent(requireActivity(), MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
     }
 }
